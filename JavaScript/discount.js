5d338c3b93692d3dc5cf2fdd8577b52100e311dc
MURADALSHORMAN <MURADALSHORMAN@GMAIL.COM>
2021-04-04 21:31:00 +0200

    murad

---
+++ JavaScript/discount.js
@@ -0,0 +1,52 @@
+
+let applydiscount;
+let totatAfter;
+let totaldiscount = document.getElementById('coupon1');
+totaldiscount.addEventListener('submit', discountclick);
+
+
+
+function discountclick(event) {
+    event.preventDefault();
+    
+
+    //////////////////////////////////////////////////
+    let n = document.getElementById('coupon_code');
+    let entercoupon = n.value * 1;
+    console.log(entercoupon);
+
+    let price = document.getElementById('totalNumber');
+    let praiceArry = price.textContent.split(' ');
+    let totalpraiceArry = praiceArry[0] * 1;
+    console.log(totalpraiceArry);
+    /////////////////////////////////////////////
+
+
+    // console.log(finaltotalprice);
+    let storecoupon = localStorage.getItem('discount') * 1;
+    console.log(storecoupon);
+    if (entercoupon === storecoupon) {
+
+        if (applydiscount < 50000) {
+            totatAfter = totalpraiceArry - (totalpraiceArry * 0.2);
+            // swal(`you have total  ${Math.ceil(totalpraiceArry * 0.2)} discount!`, `The Total price will be ${Math.ceil(totatAfter)} `, "");
+            // console.log(totatAfter);
+            newprice.textContent=`Total Discount ${Math.ceil(totalpraiceArry * .2)} JD price will be  ${Math.ceil(totatAfter)} JD `;
+        }
+        else {
+            totatAfter = totalpraiceArry - (totalpraiceArry * 0.4);
+            // swal(`you have total  ${Math.ceil(totalpraiceArry * .4)} discount!`, `The Total price will be ${Math.ceil(totatAfter)} `, "");
+            let newprice=document.getElementById('totalNumber');
+            newprice.textContent=`Total Discount ${Math.ceil(totalpraiceArry * .4)} JD price will be  ${Math.ceil(totatAfter)}  JD `;
+        }
+        localStorage.removeItem('discount');
+
+    }
+    ///////////////////////////////////////////////
+    else {
+        swal(`please Enter valid Coupon Number`, ``, "");
+    }
+    
+
+}
+// discountclick()
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,14 +5,35 @@ function showSlides() {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
+
+// 
+
+///defind the random number
+let randomValu;
+let discount;
+let sale = [];
+
+function randomNumber() {
+  randomValu = Math.floor(Math.random() * 100000) + 1;
+  return randomValu;
+}
+randomNumber();
+if(randomValu<50000){
+  discount='20%';
+}
+else{
+  discount='40%';
+}
+localStorage.setItem('discount',randomValu);
+swal(`you have ${discount} discount!`, `you can use the ${randomValu} code to get the discount for one Time ` , "");
