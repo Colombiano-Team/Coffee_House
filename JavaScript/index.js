5e10bda87255af7386645328b9f15f0211dbf53b
Ibrahim Abu-awad <ibrahimabuawadwork@gmail.com>
2021-04-04 23:08:06 +0300

    Ibrahim8

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -15,4 +15,30 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 6000); // Change image every 2 seconds
+  
 }
+
+
+
+
+
+
+
+///defind the random number
+let randomValu;
+let discount;
+let sale = [];
+
+function randomNumber() {
+  randomValu = Math.floor(Math.random() * 100000) + 1;
+  return randomValu;
+}
+randomNumber();
+if(randomValu<50000){
+  discount='20%';
+}
+else{
+  discount='40%';
+}
+localStorage.setItem('discount',randomValu);
+swal(`you have ${discount} discount!`, `you can use the ${randomValu} code to get the discount for one Time ` , ""); 
